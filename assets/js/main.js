e78786fab729245985952a9bf279ff09f19a580d
maxirosanda <maxi_rosanda@hotmail.com>
2022-12-05 16:53:04 -0300

    btn opcion 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ for (let index = 1; index <= 5; index++) {
     })
     
 }
+
 let count = 0
 document.querySelector(".shopping button").addEventListener("click",function(){
     count++
@@ -15,3 +16,27 @@ document.querySelector(".shopping button").addEventListener("click",function(){
     element.offsetWidth; 
     element.classList.add("animate");
 })
+
+let element = document.querySelector(".navbar2 ul")
+document.querySelector(".navbar2 .menu").addEventListener("click",function(){
+    
+    if(element.className == "show"){ 
+        element.classList.add("hidden");
+        element.classList.remove("show")
+    }else{
+        element.classList.add("show");
+        element.classList.remove("hidden");
+    }
+ 
+})
+window.addEventListener('resize',function(){
+    if (window.screen.width > 578){
+        element.classList.remove("hidden")
+        element.classList.remove("show")
+    }
+})
+    
+
+   
+  
+
